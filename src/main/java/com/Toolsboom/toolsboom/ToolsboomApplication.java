e00b0e159d362abfc1f05db4d5dd57ef7b08c4d1
sekercih <hkkskrc@gmail.com>
2021-06-06 23:23:16 +0300

    create

---
+++ src/main/java/com/Toolsboom/toolsboom/ToolsboomApplication.java
@@ -0,0 +1,13 @@
+package com.Toolsboom.toolsboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsboomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsboomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Toolsboom/toolsboom/ToolsboomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Toolsboom.toolsboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsboomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
